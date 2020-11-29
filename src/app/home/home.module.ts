7bb390901885a47889a3dc551265f0f9527fac20
Rolath <joona.hyttinen@gmail.com>
2020-11-29 11:37:03 +0200

    First commit

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { NgCircleProgressModule } from "ng-circle-progress";
+import { HomePage } from './home.page';
+
+import { HomePageRoutingModule } from './home-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    HomePageRoutingModule,
+    NgCircleProgressModule.forRoot({
+      radius: 100,
+      outerStrokeWidth: 16,
+      innerStrokeWidth: 8,
+      outerStrokeColor: "#008000",
+      innerStrokeColor: "#8DB600",
+      animation: false,
+      responsive: true,
+      renderOnClick: false,
+    }),
+  ],
+  declarations: [HomePage],
+})
+export class HomePageModule {}
